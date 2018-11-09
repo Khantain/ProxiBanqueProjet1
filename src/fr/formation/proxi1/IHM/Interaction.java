c7a6be1e063988b7489fb24a6b46e8434677343a
Khantain <quentinjouhault@gmail.com>
2018-11-09 11:43:09 +0100

    mise a plat pour benoit

--- src/fr/formation/proxi1/IHM/Interaction.java
+++ src/fr/formation/proxi1/IHM/Interaction.java
@@ -2,8 +2,6 @@ package fr.formation.proxi1.IHM;
 
 import java.util.Scanner;
 
-import fr.formation.proxi1.Applicative.Gestion;
-import fr.formation.proxi1.metier.Conseiller;
 
 public class Interaction {
 
@@ -16,53 +14,31 @@ public class Interaction {
 		String input = scanner.nextLine();
 //		scanner.close();	
 		return input;
-<<<<<<< HEAD
+
 	}	
 			
-	public void mainMenu(Conseiller conseiller) {
+	public String mainMenu() {
+		
+		String reponse = "";
+		do {
 		this.display("----------ProxiBanqueSI----------\n");		
 		this.display("----------Menu principal----------\n");
 		this.display("\t" + "1 - Creer un client");
 		this.display("\t" + "2 - Gerer client");
-		this.display("\t" + "3 - Quitter le programme");		
-		String userInput = this.read();
-		if (userInput.equals("1")) {
-			Gestion gestion = new Gestion();
-			gestion.creerClient(conseiller);		
-		}
-		else if (userInput.equals("2")) {
-			this.Menugererclient(conseiller);
-		}
-		else if (userInput.equals("3")) {
-			System.exit(0);
-		}
-		else {
-			this.display("Saisie erronee, 1, 2 ou 3 attendu. \n");
-			this.mainMenu(conseiller);
-		}
-=======
->>>>>>> 05c348a088c2478c7100e6815692d8ded85e4740
-	}
-
-	public String mainMenu() {
-		String reponse = "";
-		do {
-			this.display("----------ProxiBanqueSI----------" + "\n");
-			this.display("----------Menu principal----------");
-			this.display("\t" + "1 - Creer un client");
-			this.display("\t" + "2 - Gerer client");
-			this.display("\t" + "3 - Quitter le programme");
-			reponse = this.read();
-		} while (!reponse.equals("1") && !reponse.equals("2") &&!reponse.equals("3"));
+		this.display("\t" + "3 - Quitter le programme");	
+		reponse = this.read();
+		
+		} while (!reponse.equals("1") && !reponse.equals("2") && !reponse.equals("3"));
 		
 		return reponse;
+
 	}
+
 
 	public String Menugererclient() {
 		String reponse = "";
 		do {
 		this.display("");
-<<<<<<< HEAD
 		this.display("----------Menu de gestion de client----------\n");
 		this.display("\t1 - Lister information client");
 		this.display("\t2 - Modifier information client");		
@@ -71,53 +47,11 @@ public class Interaction {
 		this.display("\t5 - Faire simulation");
 		this.display("\t6 - Retour menu principal");
 		this.display("\t7 - Quitter le programme");
-=======
-		this.display("----------Menu de gestion de client----------");
-		this.display("\t" + "1 - Lister information client");
-		this.display("\t" + "2 - Modifier information client");
-		this.display("\t" + "3 - Supprimer un client");
-		this.display("\t" + "4 - Faire virement");
-		this.display("\t" + "5 - Faire simulation");
-		this.display("\t" + "6 - Retour menu principal");
-		this.display("\t" + "7 - Quitter le programme");
-<<<<<<< HEAD
+
 		
 		} while (!reponse.equals("1") && !reponse.equals("2") && !reponse.equals("3") && !reponse.equals("4") && !reponse.equals("5") && !reponse.equals("6") && !reponse.equals("7"));
 		
 		return reponse;
-=======
->>>>>>> 05c348a088c2478c7100e6815692d8ded85e4740
-		String userInput = this.read();
-		if (userInput.equals("1")) {
-			Gestion gestion = new Gestion();
-//			gestion.lireInfosClient(client)
-		} else if (userInput.equals("2")) {
-			Gestion gestion = new Gestion();
-//			gestion.modifierClient(client);
-		} else if (userInput.equals("3")) {
-			Gestion gestion = new Gestion();
-//				gestion.suppressionClient(client);
-		} else if (userInput.equals("4")) {
-			Gestion gestion = new Gestion();
-//			gestion.faireVirement(clientCrediteur, clientDebiteur);
-		} else if (userInput.equals("5")) {
-			Gestion gestion = new Gestion();
-//			gestion.faireSimulation(client);
-		} else if (userInput.equals("6")) {
-			this.mainMenu();
-		} else if (userInput.equals("7")) {
-			System.exit(0);
-<<<<<<< HEAD
-		}
-		else {
-			this.display("Saisie erronee, 1, 2, 3, 4, 5 ou 6 attendu.\n");
-			this.Menugererclient(conseiller);
-=======
-		} else {
-			this.display("Saisie erronee, 1, 2, 3, 4, 5 ou 6 attendu.");
-			this.Menugererclient();
->>>>>>> 05c348a088c2478c7100e6815692d8ded85e4740
-		}
->>>>>>> b5eff0ecf5e1f3f10e203830a246c51a24154ca1
+
 	}
 }
--- src/fr/formation/proxi1/metier/Conseiller.java
+++ src/fr/formation/proxi1/metier/Conseiller.java
@@ -12,49 +12,32 @@ public class Conseiller {
 	public List<Client> clients;
 
 	public Conseiller(String id) {
-		
-		
-			this.id = id;
-			this.clients = new ArrayList<>();
-			Client client1 = new Client("Carre", "Triangle", "12 rue des Abysses",32000, "Bordeaux", 0145781934,
-					new CompteCourant("123", 3000, LocalDate.now()), new CompteEpargne("4551", 32145, LocalDate.now()), new CarteVisa("12354867","active"));
-			Client client2 = new Client("Cercle", "Alexandre", "4 avenue de Paris", 13568, "Marseille", 0123457869,
-					new CompteCourant("456", 3000, LocalDate.now()), new CompteEpargne("78415", 32145, LocalDate.now()), new CarteVisa("7841","active"));
-			this.clients.add(client1);
-			this.clients.add(client2);
-			
-<<<<<<< HEAD
-		
-		}
-		
-		else if(id.equals("JeSuisConseillerTest2")) {
-			this.id = id;
-			this.clients = new ArrayList<>();
-			Client client3 = new Client("Joli", "Jose", "58 rue des Roses",55140, "Bordeaux", 0145781934,
-					new CompteCourant("123", 3000, LocalDate.now()), new CompteEpargne("4551", 32145, LocalDate.now()), new CarteVisa("12354867","active"));
-			Client client4 = new Client("Ferc", "Bureau", "41 rue des Abysses",45128, "Plouc", 0145457812,
-					new CompteCourant("123", 3000, LocalDate.now()), new CompteEpargne("4551", 32145, LocalDate.now()), new CarteVisa("12354867","active"));
-			this.clients.add(client3);
-			this.clients.add(client4);
-		
-		}
-=======
-	
->>>>>>> 05c348a088c2478c7100e6815692d8ded85e4740
+
+		this.clients = new ArrayList<>();
+		Client client1 = new Client("Carre", "Triangle", "12 rue des Abysses", 32000, "Bordeaux", 1457819349,
+				new CompteCourant("123", 3000, LocalDate.now()), new CompteEpargne("4551", 32145, LocalDate.now()),
+				new CarteVisa("12354867", "active"));
+		Client client2 = new Client("Cercle", "Alexandre", "4 avenue de Paris", 13568, "Marseille", 1234578699,
+				new CompteCourant("456", 3000, LocalDate.now()), new CompteEpargne("78415", 32145, LocalDate.now()),
+				new CarteVisa("7841", "active"));
+		this.clients.add(client1);
+		this.clients.add(client2);
+
+
 	}
-	
-	public Conseiller(){
+
+	public Conseiller() {
 		Interaction interaction = new Interaction();
 		interaction.display("Bonjour. veuillez saisir votre nom : ");
 		this.id = interaction.read();
 		this.clients = new ArrayList<>();
-		interaction.display("Bonjour " + this.id+ ". Vous n'avez pour l'instant aucun client enregistre. \n");
-	
+		interaction.display("Bonjour " + this.id + ". Vous n'avez pour l'instant aucun client enregistre. \n");
+
 	}
 
 	@Override
 	public String toString() {
-		return ("Conseiller " + this.id+ " en charge de " + this.clients.size() + " client(s).");
+		return ("Conseiller " + this.id + " en charge de " + this.clients.size() + " client(s).");
 	}
 
 }
