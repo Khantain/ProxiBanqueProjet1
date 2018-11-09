daca69fd3c56bc2d2714f2512e982f97e9aa49cc
Khantain <quentinjouhault@gmail.com>
2018-11-09 10:08:07 +0100

    Finalisation modifs client

--- src/fr/formation/proxi1/metier/CarteBancaire.java
+++ src/fr/formation/proxi1/metier/CarteBancaire.java
@@ -20,15 +20,12 @@ public class CarteBancaire {
 		this.id = interaction.read();
 		this.status = "active";
 	}
-<<<<<<< HEAD
-	
-=======
+
 
 
 	@Override
 	public String toString() {
-		return ("Carte numero " + this.id + " . Status : " + this.status);
+		return ("Carte numero " + this.id + " . Statut : " + this.status);
 	}	
->>>>>>> b239d0f14aeb262e3098f88b19ed4e06661c1132
-	
+
 }
